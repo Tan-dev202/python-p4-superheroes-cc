dab7fbc4dae19192e00bf334f515e78fb4a0bec6
Enock Tangus <tanguskipngeno@gmail.com>
2025-06-14 14:10:55 +0300

    implement models and seed db

---
+++ server/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from flask import Flask, request, make_response
+from flask_migrate import Migrate
+from models import db, Hero, Power, HeroPower
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
