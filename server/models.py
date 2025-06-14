a49fa03c9e4f73a3fe872c4f9dab141e70131e9d
Enock Tangus <tanguskipngeno@gmail.com>
2025-06-14 16:11:09 +0300

    format autopep8

--- server/models.py
+++ server/models.py
@@ -11,48 +11,51 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Hero(db.Model, SerializerMixin):
-    __tablename__= 'heroes'
-    
+    __tablename__ = 'heroes'
+
     serialize_rules = ('-hero_powers.hero',)
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     super_name = db.Column(db.String)
-    
-    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
+
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='hero', cascade='all, delete-orphan')
 
 
 class Power(db.Model, SerializerMixin):
     __tablename__ = 'powers'
-    
+
     serialize_rules = ('-hero_powers.power',)
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    
-    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
-    
+
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='power', cascade='all, delete-orphan')
+
     @validates('description')
     def validate_description(self, key, description):
         if not description or len(description) < 20:
-            raise ValueError("Description must be present and at least 20 characters long")
+            raise ValueError(
+                "Description must be present and at least 20 characters long")
         return description
 
 
 class HeroPower(db.Model, SerializerMixin):
     __tablename__ = 'hero_powers'
-    
+
     serialize_rules = ('-hero.hero_powers', '-power.hero_powers')
-    
+
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
-    
+
     hero = db.relationship('Hero', back_populates='hero_powers')
     power = db.relationship('Power', back_populates='hero_powers')
-    
+
     @validates('strength')
     def validate_strength(self, key, strength):
         values = ['Strong', 'Weak', 'Average']
